99491dee5a6123322eba8b15be5f8524393401ca
Michal Polovka <mpolovka@redhat.com>
2019-05-20 14:31:48 +0200

    controller split

--- sport_tracker/controller/argument_actions.py
+++ sport_tracker/controller/argument_actions.py
@@ -3,8 +3,4 @@ def process_args(arguments):
     # TODO: Implement
 
 
-def add_user():
-    # use dbController with "with" statement
-    # ask for the information interactively using input, for repeating verifications write functions
-    # TODO: Implement
-    pass
+
---
+++ sport_tracker/controller/user_actions.py
@@ -0,0 +1,5 @@
+def add_user():
+    # use dbController with "with" statement
+    # ask for the information interactively using input, for repeating verifications write functions
+    # TODO: Implement
+    pass
